9265d4af00c4a8e030e7d6fe0a9a64c43b07516f
i-trace <meshjeez@gmail.com>
2015-05-20 15:53:03 +0300

    Create database.java

---
+++ database.java
@@ -0,0 +1,12 @@
+
+package AppPackage;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DBClass {
+    Connection conn=null;
+    ResultSet rs=null;
+    PreparedStatement pst=null;
+}
